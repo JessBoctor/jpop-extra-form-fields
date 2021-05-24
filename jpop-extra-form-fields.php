53271007fc685d709b06f84933080ce6b3720693
Jessica Boctor <jessicaboctor@Jessicas-MacBook-Pro.local>
2021-05-24 15:11:50 -0700

    Created the initial plugin file

---
+++ jpop-extra-form-fields.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       Jetpack Extra Form Fields
+ * Plugin URI:        https://github.com/JessBoctor/jpop-extra-form-fields
+ * Description:       Extend Jetpack forms by adding in additional form field types.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jess Boctor & Sergey Mitroshin
+ * Author URI:        https://jessboctor.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       jpop-extra-form-fields
+ * Domain Path:       /languages
+**/
